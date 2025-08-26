0e891496c1636ee28a5d6b99bce1bf0dcaa82c24
LEAKONO <leakonoemmanuel3@gmail.com>
2025-08-26 21:40:15 +0300

    feat: implement App component with API fetch and search

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import MovieCard from './components/MovieCard'
 import SearchBar from './components/SearchBar'
 function App() {
 const[movies ,setMovies]=useState([])
+const[search,setSearch]=useState([])
 useEffect(()=>{
-  fetch("")
+  fetch("https://api.tvmaze.com/shows")
+  .then(r=>r.json())
+  .then(data=>setMovies(data))
 })
+ const filtered=movies.filter(movie=>(
+  movie.name.toLowerCase().includes(setSearch.toLowerCase())
+))
   return (
-   <div>
-    <SearchBar />
-    <MovieCard/>
-   </div>
+ <div>
+  <SearchBar search={search} setSearch={setSearch}/>
+  <MovieCard movies={filtered} />
+ </div>
+  
   )
 }
 
