9e57b7f81d61ab0ce2e360e33a293c762af92c4c
LEAKONO <leakonoemmanuel3@gmail.com>
2025-08-26 22:59:37 +0300

    feat(ui): style movie cards and layout with TailwindCSS

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,14 @@
 function MovieCard({ movie }) {
   return (
-    <div>
-      <h1>{movie.name}</h1>
-      <p>{movie.genres.join(", ")}</p>
+    <div className="bg-slate-100 rounded-lg shadow p-4">
+      <h1 className="text-amber-600 font-bold">{movie.name}</h1>
+      <p className="text-amber-950">{movie.genres.join(", ")}</p>
       {movie.image ? (
-        <img src={movie.image.medium} alt={movie.name} />
+        <img src={movie.image.medium} alt={movie.name} className="w-full h-64 object-cover rounded-md mt-2" />
       ) : (
-        <p>No image available</p>  // fallback if null
+        <p className="mt-2 text-gray-500">No image available</p>
       )}
     </div>
   )
 }
+export default MovieCard;
